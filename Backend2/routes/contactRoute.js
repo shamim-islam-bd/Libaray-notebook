81f707d4164f67d73b93bd6d58c5d69dd9f59f76
Shamimzz <shamimislam5091@gmail.com>
2022-12-03 14:22:36 +0600

    added

--- Backend2/routes/contactRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { contactUs } = require("../controllers/contactController");
-const CheckAuth = require("../middleWare/authMiddleware");
-const router = express.Router();
-
-router.post("/", CheckAuth, contactUs);
-
-module.exports = router;
